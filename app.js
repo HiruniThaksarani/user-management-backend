6d481526206710e07a2b2b5ff0aa45945d810ded
Hiruni Thaksarani <hthaksarani@gmail.com>
2024-04-01 13:31:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const controller=require('./controller');
 
 app.use(cors()); //to allow cross-origin requests (different domain from the API server)
 
@@ -18,5 +19,16 @@ app.get('/', (req, res) => { // Define a route handler for the root URL
         message: 'API endpoint is working!'
     }); // Send a JSON response
 });
+
+app.get('/users', (req, res) => { // Define a route handler for the /users URL
+    const users = controller.getUsers(); // Get the list of users
+    res.json(users); // Send the list of users as a JSON response
+});
+
+app.get('/user/:id',(req,res) => { // Define a route handler for the /user/:id URL example: /user/3
+    const id=req.params.id; // Get the user ID from the URL path
+    const user =controller.getUserById(id); // Get the user details by ID
+    res.json(user); // Send the user details as a JSON response
+})
 
 module.exports =app; //Export the app object for use in other modules
